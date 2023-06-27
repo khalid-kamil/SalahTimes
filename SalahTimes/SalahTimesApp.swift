36f8b74462aa4069b409ec77e422467f74cb52d0
Khalid Kamil <khalidkamil28@gmail.com>
2023-06-27 21:07:57 +0100

    Initial Commit

---
+++ SalahTimes/SalahTimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalahTimesApp.swift
+//  SalahTimes
+//
+//  Created by Khalid Kamil on 27/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct SalahTimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
