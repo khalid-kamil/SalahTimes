6e914bbe03eec1ed8e89612f9d1c04e1ebb6b962
Khalid Kamil <khalidkamil28@gmail.com>
2023-07-19 09:51:22 +0100

    Make sample mosque from JSON from mosque calendar

--- SalahTimesTests/SalahTimesTests.swift
+++ SalahTimesTests/SalahTimesTests.swift
@@ -60,7 +60,7 @@ final class SalahTimesTests: XCTestCase {
     client.sampleCalendar = [Day(
       date: defaultDate,
       fajr: .init(start: defaultDate, congregation: defaultDate),
-      sunrise: .init(start: defaultDate, congregation: defaultDate),
+      sunrise: defaultDate,
       dhuhr: .init(start: defaultDate, congregation: defaultDate),
       asr: .init(start: defaultDate, congregation: defaultDate),
       maghrib: .init(start: defaultDate, congregation: defaultDate),
@@ -69,6 +69,19 @@ final class SalahTimesTests: XCTestCase {
     sut.loadMosque(name: "JJME")
 
     XCTAssertEqual(client.loadedMosque?.calendar.count, 1)
+  }
+
+  func test_makeSampleJsonFromMosqueCalendar() throws {
+    let sut = makeSUT().loadSampleMosque()
+
+    let encoder = JSONEncoder()
+    encoder.outputFormatting = .prettyPrinted
+    let data = try encoder.encode(sut)
+    let jsonString = try XCTUnwrap(String(data: data, encoding: .utf8))
+
+    print(jsonString)
+    print("isha cong: \(Date(timeIntervalSinceReferenceDate: 725755500))")
+    print("last day: \(Date(timeIntervalSinceReferenceDate: 725673600))")
   }
 
   // MARK: Helpers
@@ -80,17 +93,17 @@ final class SalahTimesTests: XCTestCase {
 
 // MARK: - Mosque
 
-struct Mosque {
+struct Mosque: Encodable {
   let name: String
   let calendar: [Day]
 }
 
 // MARK: - Day
 
-struct Day {
+struct Day: Encodable {
   let date: Date
   let fajr: PrayerTime
-  let sunrise: PrayerTime
+  let sunrise: Date
   let dhuhr: PrayerTime
   let asr: PrayerTime
   let maghrib: PrayerTime
@@ -99,7 +112,7 @@ struct Day {
 
 // MARK: - PrayerTime
 
-struct PrayerTime {
+struct PrayerTime: Encodable {
   let start: Date
   let congregation: Date
 }
@@ -126,6 +139,48 @@ class MosqueLoader {
     if !name.isEmpty {
       client.get(mosque: name)
     }
+  }
+
+  func loadSampleMosque() -> Mosque {
+    var calendar = [Day]()
+    let year = 2023
+    let monthsData = [
+      (1, 31),
+      (2, 28),
+      (3, 31),
+      (4, 30),
+      (5, 31),
+      (6, 30),
+      (7, 31),
+      (8, 31),
+      (9, 30),
+      (10, 31),
+      (11, 30),
+      (12, 31),
+    ]
+
+    for (month, numberOfDays) in monthsData {
+      for i in 1...numberOfDays {
+        let date = Calendar.current.date(from: DateComponents(year: year, month: month , day: i))!
+
+        let fajr = sampleSalah(of: date, startHour: 3, startMinute: i, jamaahHour: 4, jamaahMinute: 15)
+        let sunrise = Calendar.current.date(bySettingHour: 5, minute: 00, second: 0, of: date)!
+        let dhuhr = sampleSalah(of: date, startHour: 12, startMinute: i, jamaahHour: 13, jamaahMinute: 30)
+        let asr = sampleSalah(of: date, startHour: 18, startMinute: i, jamaahHour: 19, jamaahMinute: 00)
+        let maghrib = sampleSalah(of: date, startHour: 21, startMinute: i, jamaahHour: 21, jamaahMinute: i + 3)
+        let isha = sampleSalah(of: date, startHour: 22, startMinute: i, jamaahHour: 22, jamaahMinute: 45)
+
+        calendar.append(Day(date: date, fajr: fajr, sunrise: sunrise, dhuhr: dhuhr, asr: asr, maghrib: maghrib, isha: isha))
+      }
+    }
+
+    return Mosque(name: "JJME", calendar: calendar)
+  }
+
+  func sampleSalah(of date: Date, startHour: Int, startMinute: Int, jamaahHour: Int, jamaahMinute: Int) -> PrayerTime {
+    let startTime = Calendar.current.date(bySettingHour: startHour, minute: startMinute, second: 0, of: date)
+    let congregationTime = Calendar.current.date(bySettingHour: jamaahHour, minute: jamaahMinute, second: 0, of: date)
+    return PrayerTime(start: startTime ?? date, congregation: congregationTime ?? date)
   }
 
   // MARK: Private
